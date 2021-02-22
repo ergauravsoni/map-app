9baaae04394b820f38279289a053c3a6e08bfe65
Gaurav Soni <45313227+ergauravsoni@users.noreply.github.com>
2021-02-22 23:25:53 +0530

    static attempt 3

--- map_app/settings.py
+++ map_app/settings.py
@@ -156,7 +156,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     
     #Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'assets'
+    STATIC_ROOT = 'staticfiles'
     STATICFILES_DIR = (
         os.path.join(BASE_DIR, 'static'),
     ) 
